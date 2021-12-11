c71655b219d543365de71f0f7932ac1f5015fcf9
C Bares <colinedwinbares@gmail.com>
2021-12-11 13:50:12 -0600

    built timer functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
 var timeLeftEl = document.querySelector('header span');
-var timeLeft = '60';
-timeLeftEl.textContent += timeLeft;
+var timeLeft = 10;
+var startBtnEl = document.querySelector('#start');
+
+// starts timer on click
+startBtnEl.addEventListener('click', countdown);
+
+// sets timer to start counting down
+function countdown() {
+    let timer = setInterval(function() {
+        timeLeft -= 1;
+        timeLeftEl.textContent = `Time left: ${timeLeft}`
+        if (!timeLeft) {
+            clearInterval(timer);
+        }
+    }, 1000)
+}
 
